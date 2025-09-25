f06bc845df6087bf4ae2f2034c30e3c35b930fb4
Brandy Cramer <brandyswords@brightveil.ai>
2025-09-25 14:25:12 -0500

    clean up

--- template.py
+++ template.py
@@ -8,10 +8,10 @@ list_files = [
     f"{project_name}/__init__.py",
     f"{project_name}/components/__init.py",
     f"{project_name}/components/data_ingestion.py",
-    f"{project_name}/cmoponents/data_vaidation.py",
+    f"{project_name}/components/data_vaidation.py",
     f"{project_name}/components/data_transformation.py",
-    f"{project_name}/coponents/model_trainer.py",
-    f"{project_name}/coponents/model_evaluation.py", 
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py", 
     f"{project_name}/components/model_pusher.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/artifact_entity.py",
@@ -19,8 +19,8 @@ list_files = [
     f"{project_name}/entity/estimator.py",
     f"{project_name}/entity/s3_estimator.py",
     f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/pipelilne/prediction_pipeline.py",
-    f"{project_name}/pipelilne/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/aws_connection.py",
     f"{project_name}/configuration/mongo_db_connect.py",
@@ -36,8 +36,8 @@ list_files = [
     "requirements.txt",
     "DockerFile",
     ".dockerignore",
-    "demo.PythonFinalizationErrorsetup.PythonFinalizationErrorconfig/model.yaml",
     "config/schema.yaml",
+    "config/model.yaml"
 
 ]
 
